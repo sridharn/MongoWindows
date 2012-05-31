ca959afee6414f9f6808ce9d8554b869c0d32f90
sridharn <sridhar@10gen.com>
2012-05-31 01:09:39 -0700

    Stand alone MVC app to work against MongoDB

---
+++ SampleApp/MvcMovie/ObjectIdBinder.cs
@@ -0,0 +1,17 @@
+﻿namespace MvcMovie
+{
+
+    using MongoDB.Bson;
+    
+    using System;
+    using System.Web.Mvc;
+
+    public class ObjectIdBinder : IModelBinder
+    {
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            var result = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+            return new ObjectId(result.AttemptedValue);
+        }
+    }
+}
